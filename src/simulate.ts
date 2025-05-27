f9cfe321d86acdebb103fd1853b074ce63024ca6
David Kerber <dave@kerber.io>
2025-05-26 22:10:34 -0500

    feat: Add check for trust policy for all STS assume role actions.

--- src/simulate.ts
+++ src/simulate.ts
@@ -19,6 +19,12 @@ export interface SimulationRequest {
   customContextKeys: ContextKeys
 }
 
+const assumeRoleActions = new Set([
+  'sts:assumerole',
+  'sts:assumerolewithwebidentity',
+  'sts:assumerolewithsaml'
+])
+
 export async function simulateRequest(
   simulationRequest: SimulationRequest,
   collectClient: IamCollectClient
@@ -54,7 +60,7 @@ export async function simulateRequest(
     simulationRequest.resourceArn
   )
 
-  if (simulationRequest.action.toLowerCase() === 'sts:assumerole' && !resourcePolicy) {
+  if (assumeRoleActions.has(simulationRequest.action.toLowerCase()) && !resourcePolicy) {
     throw new Error(
       `Trust policy not found for resource ${simulationRequest.resourceArn}. sts:AssumeRole requires a trust policy.`
     )
