fa484138675e392b55bee571aa4732c1d3375413
David Kerber <dave@kerber.io>
2026-02-25 16:38:40 -0600

    feat: Allow overriding the folder that has worker scripts

--- src/utils/workerScriptEsm.ts
+++ src/utils/workerScriptEsm.ts
@@ -2,6 +2,9 @@ import { join, resolve } from 'path'
 
 //@ts-ignore
 let root = resolve(import.meta.url.replace('file://', ''), '../../')
+if (process.env.IAM_LENS_WORKER_ROOT) {
+  root = process.env.IAM_LENS_WORKER_ROOT
+}
 
 /**
  * Get the path to a worker script, adjusted for the build output location.
